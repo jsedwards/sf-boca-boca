c8d41bdcb3af721245e045d4ccbd77eaf5c11223
Jeff Edwards <js_edwards@yahoo.com>
2013-12-09 21:27:41 -0800

    added db seed file with one restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+a = Restaurant.new
+a.name = "Nopa"
+a.description = "A modern American classic. Situated in the North of Panhandle(NoPa) neighborhood, Nopa serves an incredible limited selection of great food. The chicken, vegetarian tagine and burger are all classics worthy of a permanent place on this stellar menu."
+a.address = "Hayes and Fell"
+a.telephone = "415-111-1111"
+a.hours = "6PM-Midnight, 7 days a week"
+a.reservations = "Call a few weeks in advance. Limited tables available online. Full dinner available at community table and bar."
+a.cuisine = "Modern American Classic"
+a.save
