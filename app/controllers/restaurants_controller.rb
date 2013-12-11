7325080cf23d42944d95cdb5110a8be561b347e2
Jeff Edwards <js_edwards@yahoo.com>
2013-12-11 13:33:18 -0800

    added lists and list partial to views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,16 @@
 class RestaurantsController < ApplicationController
+  before_action :get_lists
+
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.all 
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+  def get_lists
+    @lists = List.all
   end
 end
